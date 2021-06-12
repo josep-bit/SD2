0e7129b396bfdeae2f2abc20e3a4ffc4ea0f6d51
josep-bit <85629370+josep-bit@users.noreply.github.com>
2021-06-12 19:06:18 +0200

    fase2

--- Cos/soup.py
+++ Cos/soup.py
@@ -1,48 +1,72 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
+from lithops.storage.cloud_proxy import open
+from lithops import FunctionExecutor
+from lithops.utils import setup_lithops_logger
+
+setup_lithops_logger(logging.DEBUG)
 import re
+
 # global list
 link = []
 result = []
 
 
-class Scrapy:
-
-    def urls(self, url):
-        global link, result
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        links = soup.find_all('div', {'class': 'article-left'})
-        for i, enter in enumerate(links):
-            link.append(enter.find('a'))
-        result = map(self.separate, link)
-        return result
-
-    def stringtag(self,value):
-        return re.sub(r'<[^>]*?>', '', value)
-
-    def infototext(self, url, file):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        text = soup.find_all('p', {'data-gtm-element-container': 'modulo-texto-link', 'class': 'paragraph'})
-        for i, enter in enumerate(text):
-            p = str(enter)
-            p = self.stringtag(p)
-            with open(file, 'w') as f:
-                f.write(p)
-        f.close()
+def urls(url):
+    global link, result
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.find_all('div', {'class': 'article-left'})
+    for i, enter in enumerate(links):
+        link.append(enter.find('a'))
+    result = map(separate, link)
+    return result
 
 
-    def separate(self, tag):
-        return str(tag).split('"')[1]
+def stringtag(value):
+    return re.sub(r'<[^>]*?>', '', value)
 
-    def getlink(self, url):
-        for i in range(1, 40):
-            urls = url + str(i)
-            self.urls(urls)
+
+def infototext(url):
+    t = []
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    text = soup.find_all('p', {'data-gtm-element-container': 'modulo-texto-link', 'class': 'paragraph'})
+    for i, enter in enumerate(text):
+        p = str(enter)
+        p = stringtag(p)
+        t.append(p)
+    return t
+
+
+def getfile(t, file):
+    with open(file, 'w') as f:
+        for i in t:
+            f.write(i + '\n')
+    ok = 'ok'
+    return ok
+
+
+def separate(tag):
+    return str(tag).split('"')[1]
+
+
+def getlink(url):
+    for i in range(1, 40):
+        x = url + str(i)
+        urls(x)
 
 
 if __name__ == '__main__':
-    s = Scrapy()
-    url = 'https://www.lavanguardia.com/television/20210611/7521629/lagrimas-directo-iker-jimenez-muerte-anna-olivia.html'
-    s.infototext(url, 'a.txt')
+    url = 'https://stories.lavanguardia.com/search?q=covid&author=&section=&startDate=&endDate=&sort=&page='
+    getlink(url)
+    j = 1
+    for i in result:
+        print(j)
+        with FunctionExecutor() as exe:
+            t = infototext(i)
+            file = 'data' + str(j) + '.txt'
+            exe.call_async(getfile, (t, file))
+            print(exe.get_result())
+            j += 1
--- fuction/fuction.py
+++ fuction/fuction.py
@@ -1,92 +1,103 @@
 import logging
-import time, csv
-import statistics as stats
+from functools import reduce
 from lithops import Storage
 from lithops import FunctionExecutor
 from lithops.utils import setup_lithops_logger
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+analyzer = SentimentIntensityAnalyzer()
 setup_lithops_logger(logging.DEBUG)
 
-# global var
-ave = []
-mo = []
-med = []
 
-
-def getdates(bucket_name, obj_key, year, type, storage):
+def getnumberwords(bucket_name, obj_key, storage):
+    listn = []
     print('I am processing the object //{}/{}'.format(bucket_name, obj_key))
-    list = []
     data = storage.get_object(bucket_name, obj_key)
     for line in data.splitlines():
         for word in line.decode('utf-8').split(';'):
-            if type == 'units' and word.isdigit() and word != year:
-                list.append(word)
-            if type == 'country' and word.isalpha() and word != 'country' and word != 'units' and word != 'year':
-                list.append(word)
-
-    return list
+            w = split(word)
+            listn.append(len(w))
+    m = map(int, listn)
+    return reduce(lambda x, y: x + y, m)
 
 
-def average(bucket_name, obj_key, year, type, storage):
-    global ave
-    l = getdates(bucket_name, obj_key, year, type, storage)
-    r = map(int, l)
-    result = stats.mean(r)
-    result = round(result, 2)
-    ave.append(result)
-    return result
+def getnumberoflines(bucket_name, obj_key, storage):
+    listl = []
+    print('I am processing the object //{}/{}'.format(bucket_name, obj_key))
+    data = storage.get_object(bucket_name, obj_key)
+    for line in data.splitlines():
+        for word in line.decode('utf-8').split(';'):
+            listl.append(word)
+    return len(listl)
 
 
-def mode(bucket_name, obj_key, year, type, storage):
-    global med
-    l = getdates(bucket_name, obj_key, year, type, storage)
-    r = map(int, l)
-    result = stats.mode(r)
-    result = round(result, 2)
-    med.append(result)
-    return result
+def getnumberweirdcaracter(bucket_name, obj_key, storage):
+    count = 0
+    print('I am processing the object //{}/{}'.format(bucket_name, obj_key))
+    data = storage.get_object(bucket_name, obj_key)
+    for line in data.splitlines():
+        for word in line.decode('utf-8').split(';'):
+            if word.isalnum() is not True:
+                count += 1
+    return count
 
 
-def median(bucket_name, obj_key, year, type, storage):
-    global mo
-    l = getdates(bucket_name, obj_key, year, type, storage)
-    r = map(int, l)
-    result = stats.median(r)
-    result = round(result, 2)
-    mo.append(result)
-    return result
+def getfilterwordnumber(bucket_name, obj_key, covid, storage):
+    listf = []
+    print('I am processing the object //{}/{}'.format(bucket_name, obj_key))
+    data = storage.get_object(bucket_name, obj_key)
+    for line in data.splitlines():
+        for word in line.decode('utf-8').split(';'):
+            w = split(word)
+            result = filter(lambda x: covid in x, w)
+            listf.append(len(list(result)))
+    m = map(int, listf)
+    return reduce(lambda x, y: x + y, m)
 
 
-def store(file):
-    global ave,med,mo
-    year = ['2000', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010',
-            '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-    with open(file, 'w') as csvfile:
-        fieldnames = ['year', 'average', 'median', 'mode']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-        writer.writeheader()
-        j = 0
-        for i in year:
-            writer.writerow({'year': i, 'average': ave[j], 'median': med[j], 'mode': mo[j]})
-            j += 1
+def sentimalAnalisi(bucket_name, obj_key, storage):
+    correct = 0
+    neutral = 0
+    negative = 0
+    file = ''
+    print('I am processing the object //{}/{}'.format(bucket_name, obj_key))
+    data = storage.get_object(bucket_name, obj_key)
+    for line in data.splitlines():
+        for word in line.decode('utf-8').split(';'):
+            vs = analyzer.polarity_scores(word)
+            if vs['compound'] >= 0.75:
+                correct += 1
+            if vs['compound'] <= 0.25:
+                negative += 1
+            else:
+                neutral += 1
+    m = max(correct, neutral, negative)
+    if m == correct:
+        file = 'Positive'
+    if m == negative:
+        file = 'Negative'
+    else:
+        file = 'Neutral'
     return file
+
+
+def split(word):
+    return str(word).split(" ")
 
 
 if __name__ == "__main__":
     storage = Storage()
-    numbers = ['2000', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010',
-               '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-    i = 0
     bucket_name = 'carbigdata2'
-    obj_key = 'car2000.csv'
-    name = 'a.csv'
+    obj_key = 'data1.txt'
+
     with FunctionExecutor() as exe:
-        while i < len(numbers) - 1:
-            start = time.time()
-            exe.call_async(average, (bucket_name, obj_key, numbers[i], 'units'))
-            exe.call_async(median, (bucket_name, obj_key, numbers[i], 'units'))
-            exe.call_async(mode, (bucket_name, obj_key, numbers[i], 'units'))
-            end = time.time()
-            obj_key = obj_key.replace(numbers[i], numbers[i + 1])
-            i += 1
-        exe.call_async(store, (name,))
+        exe.call_async(getnumberwords, (bucket_name, obj_key))
+        print(exe.get_result())
+        exe.call_async(getfilterwordnumber, (bucket_name, obj_key, 'Covid-19'))
+        print(exe.get_result())
+        exe.call_async(getnumberoflines, (bucket_name, obj_key))
+        print(exe.get_result())
+        exe.call_async(getnumberweirdcaracter, (bucket_name, obj_key))
+        print(exe.get_result())
+        exe.call_async(sentimalAnalisi, (bucket_name, obj_key))
+        print(exe.get_result())
